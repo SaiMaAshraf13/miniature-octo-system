38e9ba06892edff2c4a8792a720dbaa3235bcf32
SaiMa Ashraf <maimakhan407@gmail.com>
2017-03-18 20:00:11 +0500

    Add files via upload

---
+++ addition1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout<<"Enter 1st Number"<<endl;
+	cin>>a;
+	cout<<"Enter 2nd Number"<<endl;
+	cin>>b;
+	c=a+b;
+	cout<<"Your Result is "<<c;
+	return 0;
+}
